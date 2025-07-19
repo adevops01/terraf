2189df5987c292c7b310720cd0b548756141403b
adevops01 <128698740+adevops01@users.noreply.github.com>
2025-07-19 01:29:59 +0000

    feat: testing atlantis

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### This module is used to create a vpc
 #### required values are 
 1. CIDR BLOCK
+
+```sh
+docker run -itd -p 4000:4141 --name atlantis runatlantis/atlantis server --automerge --gh-user=adevops01 --gh-token=github_token --repo-allowlist=github.com/adevops01/*
+```
--- main.tf
+++ main.tf
@@ -1,51 +1,57 @@
-resource "aws_vpc" "vpc01" {
-  cidr_block = var.cidr_block
+# resource "aws_vpc" "vpc01" {
+#   cidr_block = var.cidr_block
 
-  tags = {
-    "Name" = "myvpc01"
-  }
-}
+#   tags = {
+#     "Name" = "myvpc01"
+#   }
+# }
 
-resource "aws_internet_gateway" "myvpc-ig" {
-  vpc_id = aws_vpc.vpc01.id
-}
+# resource "aws_internet_gateway" "myvpc-ig" {
+#   vpc_id = aws_vpc.vpc01.id
+# }
 
-resource "aws_subnet" "pub-sub01" {
-  vpc_id = aws_vpc.vpc01.id
-  cidr_block = "192.168.1.0/24"
-  availability_zone = "us-east-1b"
+# resource "aws_subnet" "pub-sub01" {
+#   vpc_id = aws_vpc.vpc01.id
+#   cidr_block = "192.168.1.0/24"
+#   availability_zone = "us-east-1b"
 
-  tags = {
-    "Name" = "pub-sub01"
-  }
-}
+#   tags = {
+#     "Name" = "pub-sub01"
+#   }
+# }
 
-resource "aws_subnet" "pub-sub02" {
-  vpc_id = aws_vpc.vpc01.id
-  cidr_block = "192.168.2.0/24"
-  availability_zone = "us-east-1a"
+# resource "aws_subnet" "pub-sub02" {
+#   vpc_id = aws_vpc.vpc01.id
+#   cidr_block = "192.168.2.0/24"
+#   availability_zone = "us-east-1a"
 
-  tags = {
-    "Name" = "pub-sub02"
-  }
-}
+#   tags = {
+#     "Name" = "pub-sub02"
+#   }
+# }
 
-resource "aws_subnet" "pri-sub01" {
-  vpc_id = aws_vpc.vpc01.id
-  cidr_block = "192.168.3.0/24"
-  availability_zone = "us-east-1a"
+# resource "aws_subnet" "pri-sub01" {
+#   vpc_id = aws_vpc.vpc01.id
+#   cidr_block = "192.168.3.0/24"
+#   availability_zone = "us-east-1a"
 
-  tags = {
-    "Name" = "pri-sub01"
-  }
-}
+#   tags = {
+#     "Name" = "pri-sub01"
+#   }
+# }
 
-resource "aws_subnet" "pri-sub02" {
-  vpc_id = aws_vpc.vpc01.id
-  cidr_block = "192.168.4.0/24"
-  availability_zone = "us-east-1b"
+# resource "aws_subnet" "pri-sub02" {
+#   vpc_id = aws_vpc.vpc01.id
+#   cidr_block = "192.168.4.0/24"
+#   availability_zone = "us-east-1b"
 
-  tags = {
-    "Name" = "pri-sub02"
-  }
-}
+#   tags = {
+#     "Name" = "pri-sub02"
+#   }
+# }
+
+
+# resource "local_file" "name" {
+#   filename = "testing"
+#   content = "this is my test content"
+# }
